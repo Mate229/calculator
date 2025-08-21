1e035551ccf7e1de626222e92a035fdd83d831fe
Mate229 <adiokhalidsalami@gmail.com>
2025-08-21 15:19:52 +0100

    Add initial UI and operate function
    Add the digits and operators button to the html files
    and syle it with css. I declare variables of the operations
    as well as the operate function.

--- script.js
+++ script.js
@@ -15,3 +15,21 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 };
+
+let firstNumber, secondNumber, operator;
+
+function operate() {
+    switch (operator) {
+        case "+":
+            return add(firstNumber, secondNumber);
+            break;
+        case "-":
+            return substract(firstNumber, secondNumber);
+            break;
+        case "*":
+            return multiply(firstNumber, secondNumber);
+            break;
+        case "/":
+            return divide(firstNumber, secondNumber);
+    }
+}
