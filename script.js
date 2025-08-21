ebe8fd39bb708543bb13cc15cf745dc5e1c36bbf
Mate229 <adiokhalidsalami@gmail.com>
2025-08-21 14:32:27 +0100

    Create functions
    I create the functions for basics maths operator
    add, substract, multiply, divide.

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("calculator")
+
+function add(a, b) {
+    return a + b;
+};
+
+function substract(a, b) {
+    return a - b;
+};
+
+function multiply(a, b) {
+    return a * b;
+};
+
+function divide(a, b) {
+    return a / b;
+};
